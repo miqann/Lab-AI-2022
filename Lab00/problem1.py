973738451da44a0884aaa84b0f790d1c58ec8444
MiQan <nmquan137@gmail.com>
2022-03-09 10:23:14 +0700

    Finish all problems

--- Lab00/problem1.py
+++ Lab00/problem1.py
@@ -1,6 +1,5 @@
 x = []
-last = 3200
-for i in range(2000, last + 1):
+for i in range(2000, 3201):
     if (i%7 == 0) and (i%5 != 0):
         x.append(str(i))
 print(','.join(x))
--- Lab00/problem2.py
+++ Lab00/problem2.py
@@ -1,12 +1,8 @@
-def dictionary(x):
-    begin = '{'
-    for i in range (1, x + 1):
-        if i != 1:
-           begin += ','
-        work = ' {i1}:{i2}'.format(i1 = i, i2 = i**2)
-        begin += work
-    begin += '}'
-    return begin
+n = int(input('Input n: '))
 
-print(dictionary(int(input('Enter n: '))))
+numberDict = {}
+for i in range(1, n+1):
+    numberDict[i] = i*i
+
+print(numberDict)
     
